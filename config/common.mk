0289888cdb3a58f1281bb817f41cc45f82cc6bff
joshuous <joshuous@gmail.com>
2017-01-12 02:31:59 +0000

    VertexOS Atlas 1.8 (OMS)

--- config/common.mk
+++ config/common.mk
@@ -194,7 +194,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PACKAGE_OVERLAYS += vendor/vertex/overlay/common
 
 # Set version
-PRODUCT_VERSION = v1.7
+PRODUCT_VERSION = v1.8
 CODE_NAME = ATLAS
 
 # Unofficial by default unless defined
@@ -202,9 +202,11 @@ ifndef VERTEX_BUILDTYPE
 	VERTEX_BUILDTYPE := UNOFFICIAL	
 endif
 
-VERTEX_VERSION := VertexOS-$(CODE_NAME)-$(PRODUCT_VERSION)-$(VERTEX_BUILDTYPE)-$(shell date -u +%Y%m%d)-$(VERTEX_BUILD)
+# Name of flashable zip
+VERTEX_VERSION := VertexOS-$(CODE_NAME)-$(PRODUCT_VERSION)-OMS-$(VERTEX_BUILDTYPE)-$(shell date -u +%Y%m%d)-$(VERTEX_BUILD)
 
-VERTEX_MODVERSION := $(CODE_NAME)-$(PRODUCT_VERSION)-$(VERTEX_BUILDTYPE)-$(shell date -u +%Y%m%d)
+# Version Shown in Settings -> About
+VERTEX_MODVERSION := $(CODE_NAME)-$(PRODUCT_VERSION)-OMS-$(VERTEX_BUILDTYPE)-$(shell date -u +%Y%m%d)
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.vertex.version=$(VERTEX_VERSION) \
