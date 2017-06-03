c749446c7d510e6f7806e671a7049b7fffd8b28e
joshuous <joshuous@gmail.com>
2017-06-03 16:45:31 +0000

    build.sh: Make it work with other devices
    User has to export BUILD_DEVICE in his environment variables.

--- tools/build.sh
+++ tools/build.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
 echo "Choose build type:"
-select choice in OFFICIAL-user TEST-user EXPERIMENTAL-user TEST-userdebug EXPERIMENTAL-userdebug TEST-eng EXPERIMENTAL-eng
+select choice in OFFICIAL-user TEST-user TEST-userdebug TEST-eng EXPERIMENTAL-user EXPERIMENTAL-userdebug EXPERIMENTAL-eng
 do
     case "$choice" in
         "OFFICIAL-user")
             export VERTEX_BUILDTYPE=OFFICIAL
-            . build/envsetup.sh
-            lunch vertex_oneplus3-user
+            export BUILD_VARIANT=user
             find . -name "*Development*.apk" | xargs rm
             break;;
         "TEST-user")
             export VERTEX_BUILDTYPE=TEST
-            . build/envsetup.sh
-            lunch vertex_oneplus3-user
+            export BUILD_VARIANT=user
             break;;
         "TEST-userdebug")
             export VERTEX_BUILDTYPE=TEST
-            . build/envsetup.sh
-            lunch vertex_oneplus3-userdebug
+            export BUILD_VARIANT=userdebug
             break;;
         "TEST-eng")
             export VERTEX_BUILDTYPE=TEST
-            . build/envsetup.sh
-            lunch vertex_oneplus3-eng
+            export BUILD_VARIANT=eng
             break;;
         "EXPERIMENTAL-user")
             export VERTEX_BUILDTYPE=EXPERIMENTAL
-            . build/envsetup.sh
-            lunch vertex_oneplus3-user
+            export BUILD_VARIANT=user
             break;;
         "EXPERIMENTAL-userdebug")
             export VERTEX_BUILDTYPE=EXPERIMENTAL
-            . build/envsetup.sh
-            lunch vertex_oneplus3-userdebug
+            export BUILD_VARIANT=userdebug
             break;;
         "EXPERIMENTAL-eng")
             export VERTEX_BUILDTYPE=EXPERIMENTAL
-            . build/envsetup.sh
-            lunch vertex_oneplus3-eng
+            export BUILD_VARIANT=eng
             break;;
         *) echo "Invalid option. Try again!"
             ;;
     esac
 done
+
+if [[ -z "$BUILD_DEVICE" ]]; then
+    export BUILD_DEVICE=oneplus3
+fi
+
+. build/envsetup.sh
+lunch vertex_${BUILD_DEVICE}-${BUILD_VARIANT}
 
 time make bacon -j16
